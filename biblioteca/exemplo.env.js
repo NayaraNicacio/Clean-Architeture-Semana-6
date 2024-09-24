0ea649eea9ca950395ac3f602f945fc2080683e0
Nayara Nicacio <nayaranicacio42@gmail.com>
2024-09-24 00:25:54 +0000

    Adicionado arquivo de exemplo.env

---
+++ biblioteca/exemplo.env.js
@@ -0,0 +1,3 @@
+MONGO_URI= "Caminho para o banco de dados"
+PORT= "Porta que o banco ira rodar"
+JWT_SECRET="Senha"
--- biblioteca/model/user.js
+++ biblioteca/model/user.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose'; // ES MODULE
+// import mongoose from 'mongoose'; // ES MODULE
 
-const userSchema = new mongoose.Schema({
-    email: { type: String, unique: true },
-    username: { type: String },
-    password: { type: String },
-    watchList: { type: Array }
-});
+// const userSchema = new mongoose.Schema({
+//     email: { type: String, unique: true },
+//     username: { type: String },
+//     password: { type: String },
+//     watchList: { type: Array }
+// });
 
-const User = mongoose.model('User', userSchema);
+// const User = mongoose.model('User', userSchema);
 
-export default User; // ES Module
+// export default User; // ES Module
